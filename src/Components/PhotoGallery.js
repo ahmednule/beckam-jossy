374d5f4bb2bc8cdc8bede064720bcdb7e9aa7e5f
ahmednule <nuleahmed6@gmail.com>
2024-10-11 17:16:53 +0300

    update: fixed the unresponding images, now they are okay

--- src/Components/PhotoGallery.js
+++ src/Components/PhotoGallery.js
@@ -2,22 +2,71 @@ import React from 'react'
 import './PhotoGallery.css'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
+
+
 function PhotoGallery() {
     return (
         <div className='photogallery-container'>
             <h1 className='photogallery-title'>Photo Gallery</h1>
             <div className="photogallery-images">
-                <img src="https://unsplash.com/photos/view-of-floating-open-book-from-stacked-books-in-library-HH4WBGNyltc" alt='picture 1'/>
-                <img src="https://source.unsplash.com/1300x1200/?lebanon" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?qatar" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?uae" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?kuwait" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?oman" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?turkey" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?iran" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?jordan" alt=''/>
-                <img src="https://source.unsplash.com/1300x1200/?Kuwait" alt=''/>
-            </div>
+            <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
+            alt=""
+          ></img> 
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfRHNwRyPkTxnMOzOvv5dOK4OS_lq4-2Yugg&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
+            alt=""
+          ></img>
+          <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS34iIDoKVXOhKhdwsiGSLc9RJmtq_lSQDig&usqp=CAU"
+            alt=""
+          ></img>
+          <img className='recent-book' src='https://19en282jw7pc3zpwj22pg8v0-wpengine.netdna-ssl.com/wp-content/uploads/2021/01/Good-to-Great-Jim-Collins.jpg' alt=''></img>
+
+            <img
+            className="popular-book"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7ElcNy_v2Ri1H3VhYjYP1MzR6zBUwFQWbOirCkaqcfOqJnbrK5ZvdZNUwEfrlmJwn7pA&usqp=CAU"
+            alt=""
+          ></img>
+        </div>
             <button>VIEW MORE<ArrowForwardIosIcon style={{fontSize:20}}/></button>
         </div>
     )
